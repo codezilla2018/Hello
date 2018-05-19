0a37e3ac8fccbe078df87c6a81acedb5f7c250d5
Ravindu Sachintha <ravindusachintha53@gmail.com>
2018-05-19 14:10:41 +0530

    Update business card page

--- src/pages/business-card/business-card.ts
+++ src/pages/business-card/business-card.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { FirebaseProvider } from './../../providers/firebase/firebase';
+import { FirebaseListObservable } from 'angularfire2/database-deprecated';
 
 /**
  * Generated class for the BusinessCardPage page.
@@ -15,11 +17,35 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class BusinessCardPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  cards: FirebaseListObservable<any[]>;
+  company_name: string;
+  company_tagline: string;
+  first_name: string;
+  last_name: string;
+  title: string;
+  address_line1: string;
+  address_line2: string;
+  contact: string;
+  email: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public firebaseProvider: FirebaseProvider) {
+    this.cards = this.firebaseProvider.getCards();
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad BusinessCardPage');
+  addCardToDB(){
+    this.firebaseProvider.addCard(
+      this.company_name,
+      this.company_tagline,
+      this.first_name,
+      this.last_name,
+      this.title,
+      this.address_line1,
+      this.address_line2,
+      this.contact,
+      this.email
+    );
   }
+
+
 
 }
--- src/providers/firebase/firebase.ts
+++ src/providers/firebase/firebase.ts
@@ -13,6 +13,26 @@ export class FirebaseProvider {
   constructor(public afd: AngularFireDatabase) {
   }
 
+  getCards() {
+    return this.afd.list('/Cards/');
+  }
+
+  addCard(com_name, com_tagline, fname, lname, ttl, add_line1, add_line2, con_no, eml) {
+    this.afd.list('/Cards/').push({
+      company_name: com_name,
+      company_tagline: com_tagline,
+      first_name: fname,
+      last_name: lname,
+      title: ttl, 
+      address_line1: add_line1, 
+      address_line2: add_line2, 
+      contact_no: con_no, 
+      email: eml
+    });
+  }
+
+
+
   getUsers() {
     return this.afd.list('/users/');
   }
